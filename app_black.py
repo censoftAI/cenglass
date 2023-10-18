3efe46813cf0126ba63f9ac1b9a15c076a30a6e3
gbox3d <gbox3d@gmail.com>
2023-10-18 19:58:24 +0900

    ....

--- app_black.py
+++ app_black.py
@@ -7,7 +7,7 @@ import yaml
 import time
 import pygame
 import numpy as np
-
+# from dotenv import load_dotenv
 import threading
 import queue
 
@@ -74,6 +74,8 @@ def inference_task():
         
         msg_queue.put('AI Model Ready')
         
+        conf_threshold = config['CONF_THRESHOLD']
+        
         while shutdown_event.is_set() == False :
             try :
                 success, frame = cap_data_queue.get(block=False)
@@ -82,7 +84,7 @@ def inference_task():
                 continue
             # print(f'cap_data_queue success : {success}')
             if success:
-                results = model(source=frame, conf=0.25, verbose=False)
+                results = model(source=frame, conf=conf_threshold, verbose=False)
                 data_queue.put((results,class_names))  # 추론 결과를 큐에 넣기
         
     except Exception as e:
@@ -92,6 +94,10 @@ def inference_task():
     
     print('inference_task end')
         
+def draw_crosshair(screen, color, size,x,y):
+    pygame.draw.line(screen, color, (x - size, y), (x + size, y), 1)
+    pygame.draw.line(screen, color, (x, y - size), (x, y + size), 1)
+    
 # 화면 랜더링 태스크
 def rendering_task():
     
@@ -132,8 +138,19 @@ def rendering_task():
     else:
         screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
     
-    font = pygame.font.Font(None, 36)
+    # font 설정
+    # font = pygame.font.Font("path/to/your/font.ttf", 30) 
+    title_font = pygame.font.Font('./font/NanumGothic.ttf', 36)
     msg_font = pygame.font.Font(None, 24)
+    
+    # 설명서 이미지 로드
+    canon_img = pygame.image.load("./res/canon.jpg")
+    canon_img = pygame.transform.scale(canon_img, (screen_width/2, screen_height))
+    heli_img = pygame.image.load("./res/heli.jpg")
+    heli_img = pygame.transform.scale(heli_img, (screen_width/2, screen_height))
+    wemosd1_img = pygame.image.load("./res/wemosd1.png")
+    wemosd1_img = pygame.transform.scale(wemosd1_img, (screen_width/2, screen_height))
+    
     _prev_msg_text = None    
     _prev_result = None
     break_flag = False
@@ -188,44 +205,58 @@ def rendering_task():
             screen.fill(black)
 
         # 가운데 십자표시 그리기
-        pygame.draw.line(screen, white, (screen_width // 2, 0), (screen_width // 2, screen_height), 1)
-        pygame.draw.line(screen, white, (0, screen_height // 2), (screen_width, screen_height // 2), 1)
+        # pygame.draw.line(screen, white, (screen_width // 2, 0), (screen_width // 2, screen_height), 1)
+        # pygame.draw.line(screen, white, (0, screen_height // 2), (screen_width, screen_height // 2), 1)
+        draw_crosshair(screen, white, 64, screen_width // 2, screen_height // 2)
         
-        # Draw the bounding boxes on the screen
+        drawn_boxes = []  # 이 리스트에는 그려진 박스의 좌표를 저장합니다.
+        # 카메라 해상도와 스크린해상도가 다를 경우 위치 보정
+        scalex = screen_width / frame_width
+        scaley = screen_height / frame_height
+        
+        # 화면의 중점 좌표
+        center_x, center_y = screen_width // 2, screen_height // 2
+        # drawn_boxes 초기화
+        drawn_boxes = []
+
         for result in results:
             boxes = result.boxes
             for box in boxes:
-                x1, y1, x2, y2 = box.xyxy[0]
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-                
-                # 카메라 해상도와 스크린해상도가 다를 경우 위치 보정
-                scalex = screen_width / frame_width
-                scaley = screen_height / frame_height
-                
-                x1, y1, x2, y2 = int(x1 * scalex), int(y1 * scaley), int(x2 * scalex), int(y2 * scaley)
-                
-                # 사각형 그리기
-                # pygame.draw.rect(screen, green, (x1, y1, x2 - x1, y2 - y1), 2)
-                
-                # 클래스 이름과 신뢰도 출력
+                # Get class and confidence
                 class_id = int(box.cls.cpu().item())
                 class_name = class_names[class_id]
-                conf = int(box.conf.cpu().item() * 100)
                 
-                label_text = f"{class_name} {conf}%"
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                x1, y1, x2, y2 = int(x1 * scalex), int(y1 * scaley), int(x2 * scalex), int(y2 * scaley)
                 
-                text_surface = font.render(label_text, True, green)
+                # 겹치는 박스가 있는지 확인
+                overlap = False
+                for drawn_box in drawn_boxes:
+                    dx1, dy1, dx2, dy2 = drawn_box
+                    if x1 < dx2 and x2 > dx1 and y1 < dy2 and y2 > dy1:
+                        overlap = True
+                        break
                 
-                # 중심에 출력
-                text_width, text_height = text_surface.get_size()
-                center_x = (x1 + x2) / 2 - text_width / 2
-                center_y = (y1 + y2) / 2 - text_height / 2
-                screen.blit(text_surface, (center_x, center_y))
+                # 겹치는 박스가 없으면 그립니다.
+                if not overlap:
+                    pygame.draw.rect(screen, green, (x1, y1, x2 - x1, y2 - y1), 2)
+                    drawn_boxes.append((x1, y1, x2, y2))
 
+                    # 화면의 중점이 박스 안에 있는지 확인
+                    if x1 < center_x < x2 and y1 < center_y < y2:
+                        # 이미지 출력
+                        # img_width, img_height = canon_img.get_size()
+                        if class_name == 'canon':
+                            screen.blit(canon_img, (center_x, 0))
+                        elif class_name == 'heli':
+                            screen.blit(heli_img, (center_x, 0))
+                        elif class_name == 'wemosd1':
+                            screen.blit(wemosd1_img, (center_x, 0))
+                        break
                 
         try :
             _msg_text = msg_queue.get(block=False)
-            
         except queue.Empty:
             if _prev_msg_text != None:
                 _msg_text = _prev_msg_text
@@ -245,6 +276,13 @@ def rendering_task():
         fps_surface = msg_font.render(fps_text, True, green)
         screen.blit(fps_surface, (10, 25))  # Adjust the position (10, 10) as needed
         
+        # 타이틀이 존재하면 상단 중앙에 타이틀 출력
+        if config['TITLE'] != None:
+            title_text = config['TITLE']
+            title_surface = title_font.render(title_text, True, green)
+            title_width, title_height = title_surface.get_size()
+            screen.blit(title_surface, (screen_width / 2 - title_width / 2, 0))
+        
         pygame.display.update()
 
         # Pygame 이벤트 처리 (종료 등)
@@ -261,16 +299,13 @@ def rendering_task():
 inference_thread = threading.Thread(target=inference_task)
 inference_thread.start()
 
-
-#time.sleep(30)
+#%% 메인 루프
 print('start rendering_task')
 rendering_task()
 
 time.sleep(1)
 # 종료 이벤트 설정 (이 호출은 쓰레드에게 종료할 것임을 알립니다.)
 shutdown_event.set()
-
 # 스레드가 종료될 때까지 대기
 inference_thread.join()
-
 print('Done.')
